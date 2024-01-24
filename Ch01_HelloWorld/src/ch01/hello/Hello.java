da0c0d6a2b0d10985eabcb6be489c789db15b486
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 11:31:41 +0900

    Hello, Hava 출력 추가

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 //	 프로그램에서 프로그램 시작을 main으로 정해두었다.
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		System.out.println("Hello, Java");
 	}
 }
  
