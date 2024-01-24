3ebd32dd0b885aa9e9e22c5a9edc2c0ffc572c06
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 11:27:44 +0900

    ch01_HelloWorld 프로젝트 생성

---
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -0,0 +1,9 @@
+package ch01.hello;	// 소속 -> 동일이름 클래스 구분가능
+
+public class Hello {
+//	 프로그램에서 프로그램 시작을 main으로 정해두었다.
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
+ 
